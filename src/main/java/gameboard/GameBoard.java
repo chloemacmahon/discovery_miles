7882bfeb2a48b216f05ff3468c3633f50e87adc5
Chloe <chloemacmahon@gmail.com>
2021-08-19 10:33:06 +0200

    Initial Commit

--- src/main/java/gameboard/GameBoard.java
+++ src/main/java/gameboard/GameBoard.java
@@ -4,15 +4,19 @@ import java.util.List;
 
 public class GameBoard {
     private List<List<GameTile>> gameBoard;
+    private int amountOfRows;
+    private int getAmountOfColumns;
     private int tilesRevealed;
 
-    public GameBoard(List<List<GameTile>> gameBoard, int tilesRevealed) {
+    public GameBoard(List<List<GameTile>> gameBoard, int amountOfRows, int getAmountOfColumns, int tilesRevealed) {
         this.gameBoard = gameBoard;
+        this.amountOfRows = amountOfRows;
+        this.getAmountOfColumns = getAmountOfColumns;
         this.tilesRevealed = tilesRevealed;
     }
 
     public int revealTile(int rowNumber, int columnNumber){
-
+        if
     }
 
     public List<List<GameTile>> getGameBoard() {
@@ -29,5 +33,21 @@ public class GameBoard {
 
     public void setTilesRevealed(int tilesRevealed) {
         this.tilesRevealed = tilesRevealed;
+    }
+
+    public int getGetAmountOfColumns() {
+        return getAmountOfColumns;
+    }
+
+    public void setGetAmountOfColumns(int getAmountOfColumns) {
+        this.getAmountOfColumns = getAmountOfColumns;
+    }
+
+    public int getAmountOfRows() {
+        return amountOfRows;
+    }
+
+    public void setAmountOfRows(int amountOfRows) {
+        this.amountOfRows = amountOfRows;
     }
 }
